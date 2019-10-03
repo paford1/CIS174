342602608eb0fface96149362f2de80e9ddaee53
Paul Ford <paford1@dmacc.edu>
2019-10-02 22:11:59 -0500

    adding assignment 5.1

---
+++ Assignment_5_1_StarterCode/CIS174_TestCoreApp/Models/CreatePersonViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class CreatePersonViewModel
+    {
+        [Required]
+        [StringLength(25)]
+        [Display (Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(25)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range (1,120)]
+        public int Age { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        [Compare(nameof(Email))]
+        public string ConfirmEmail { get; set; }
+
+        [MinLength(7)]
+        [Url]
+        public string Website { get; set; }
+        
+        [Editable (false, AllowInitialValue = true)]
+        [ReadOnly (true)]
+        public string School { get;  set; }
+    }
+}
